d74be4f87fa0fbdc74141d2d83ea0318d3a33903
jesswhyte <jess.whyte@gmail.com>
2018-01-25 17:43:32 -0500

    photograph media

--- floppycapture.py
+++ floppycapture.py
@@ -1,9 +1,11 @@
-# Python script to walk through floppy disk capture workflow
-# Jess, Jan 2018
+### IN PROGRESS
+### Python script to walk through floppy disk capture workflow
+### Jess, Jan 2018
 
 import sys
 import argparse 
 import os
+import subprocess
 
 ############### CHANGE THE LIBRARY #############
 # For list of library IDs, visit: uoft.me/libs #
@@ -11,7 +13,7 @@ import os
 lib = "ECSL"
 drive = "d0"
 
-# Get arguments
+### Get arguments
 parser = argparse.ArgumentParser(
 	description ="Script to walk through floppy disk capture workflow, Jan 2018")
 parser.add_argument(
@@ -25,29 +27,18 @@ parser.add_argument(
 #parser.add_argument(
 #	'-l', '--label', type=str, help='Transcript of label', required=False
 
-# Array for all args passed to script
+### Array for all args passed to script
 args = parser.parse_args()
 
 
-# Define variables
+### Define variables
 mediaType = args.mediatype
 #totalDisks = args.number
 callNum = args.call
 
-
-## Functions
-
-def checkMedia():
-	if mediaType == "3.5": 
-		metadata.write("\n" + "Media: " + mediaType + " floppy disk")
-		drive = "d0"
-	elif mediaType == "5.25":
-		metadata.write("\n" + "Media: " + mediaType + " floppy disk")
-		drive = "d1"
-	else:
-		print "\n" + "Incorrect media type entered, please use 3.5 or 5.25"
-		raise SystemExit
-
+#######################
+###### FUNCTIONS ######
+#######################
 
 #def getDiskId():	
 #	if totalDisks == 1
@@ -60,16 +51,37 @@ def kfStream():
 	os.system("dtc -" + drive + " -f"  +callNum+"_stream -i0")
 	print "KF in progress..."
 
-# Open our metadata.txt file
+########################
+#####  THE GOODS  ######
+########################
+
+### Open our metadata.txt file
 metadata = open('TEMPmetadata.txt','w')
 
-# Open master log file, appendable, create if it doesn't exist
+### Open master log file, appendable, create if it doesn't exist
 log = open('log.csv','a+')
 
-# What type of media is it? checkMedia function
-checkMedia()
-#get a preservation stream
-kfStream()
+### check Media 
+if mediaType == "3.5": 
+	metadata.write("\n" + "Media: " + mediaType + " floppy disk")
+	drive = "d0"
+elif mediaType == "5.25":
+	metadata.write("\n" + "Media: " + mediaType + " floppy disk")
+	drive = "d1"
+else:
+	print "\n" + "Incorrect media type entered, please use 3.5 or 5.25"
+	raise SystemExit
+
+### Take a picture
+
+picName = callNum + '_' + args.descriptor + '_pic.jpg'
+picParameters = " --jpeg 95 -r 1600x1200 --no-banner " + picName
+os.system("fswebcam"+ picParameters)
+metadata.write ("n" + "Picture: " + picName
+
+
+### Get a preservation stream
+#kfStream()
 
 
 ####################
@@ -78,12 +90,12 @@ kfStream()
 
 metadata.close()
 
-# Rename our metadata.txt file
+### Rename our metadata.txt file
 newMetadata = callNum + '_' + args.descriptor + '_metadata.txt'
 os.rename('TEMPmetadata.txt', newMetadata)
 
-# Update master log
-log.write("\n" + lib + "," + callNum + "," + mediaType + ",")
+### Update master log
+log.write("\n" + lib + "," + callNum + "," + mediaType + "," + picName + ",")
 
-# Close master log
+### Close master log
 log.close()
