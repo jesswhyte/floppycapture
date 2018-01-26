4976c23d4adf0765c36617ddc728735293d05c5e
jesswhyte <jess.whyte@gmail.com>
2018-01-26 15:13:14 -0500

    add args

--- floppycapture.py
+++ floppycapture.py
@@ -6,35 +6,39 @@ import sys
 import argparse 
 import os
 import subprocess
+import datetime
 
 ############### CHANGE THE LIBRARY #############
 # For list of library IDs, visit: uoft.me/libs #
 ################################################ 
 lib = "ECSL"
-drive = "d0"
 
 ### Get arguments
 parser = argparse.ArgumentParser(
 	description ="Script to walk through floppy disk capture workflow, Jan 2018")
 parser.add_argument(
-	'-m', '--mediatype', type=str, help='Use \"3.5\" or \"5.25\"',required=True)
+	'-m', '--mediatype', type=str, help='Use \"3.5\" or \"5.25\"',required=True,
+	choices=['3.5','5.25'])
 #parser.add_argument(
 #	'-n', '--number', type=int, help='Number of disks in collection', required=True)
 parser.add_argument(
 	'-c', '--call', type=str, help='Call or Collection Number', required=False)
 parser.add_argument(
-	'-d','--descriptor', type=str, help='descriptor', required=False)
-#parser.add_argument(
-#	'-l', '--label', type=str, help='Transcript of label', required=False
+	'-d','--descriptor', type=str, help='brief descriptor', required=True)
+parser.add_argument(
+	'-l', '--label', type=str, help='Transcript of label', required=False)
 
 ### Array for all args passed to script
 args = parser.parse_args()
 
 
 ### Define variables
+drive = "d0"
+date = datetime.datetime.today().strftime('%Y-%m-%d')
 mediaType = args.mediatype
 #totalDisks = args.number
 callNum = args.call
+
 
 #######################
 ###### FUNCTIONS ######
@@ -57,9 +61,12 @@ def kfStream():
 
 ### Open our metadata.txt file
 metadata = open('TEMPmetadata.txt','w')
+metadata.write("callnumber: " + callNum)
+metadata.write("\n" + "Date of Capture: " + date)
+metadata.write("\n" + "Label Transcript: " + args.label)
 
 ### Open master log file, appendable, create if it doesn't exist
-log = open('log.csv','a+')
+log = open('projectlog.csv','a+')
 
 ### check Media 
 if mediaType == "3.5": 
@@ -72,13 +79,16 @@ else:
 	print "\n" + "Incorrect media type entered, please use 3.5 or 5.25"
 	raise SystemExit
 
-### Take a picture
+### Take a Picture
+### Note: fswebcam defaults to /dev/video0, if device not found...
+### use ls -ltrh /dev/video* to list devices and use 
+### flag -d to set device
+### use cheese or VTL42 test utility to set focus, if needed
 
 picName = callNum + '_' + args.descriptor + '_pic.jpg'
-picParameters = " --jpeg 95 -r 1600x1200 --no-banner " + picName
+picParameters = " --jpeg 95 -r 1600x1200 --no-banner -d /dev/video1 " + picName
 os.system("fswebcam"+ picParameters)
-metadata.write ("n" + "Picture: " + picName
-
+metadata.write ("\n" + "Picture: " + picName)
 
 ### Get a preservation stream
 #kfStream()
@@ -95,7 +105,7 @@ newMetadata = callNum + '_' + args.descriptor + '_metadata.txt'
 os.rename('TEMPmetadata.txt', newMetadata)
 
 ### Update master log
-log.write("\n" + lib + "," + callNum + "," + mediaType + "," + picName + ",")
+log.write("\n" + lib +","+ callNum +","+"\""+args.label +"\","+ mediaType)
 
 ### Close master log
 log.close()
